eecdaaaf6ac32156a4c6b0207e6d373293d00966
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-11-02 20:01:47 +0200

    Setup PrimeVue with TailwindCSS

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
+import Aura from '@primevue/themes/aura'
+import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import App from './App.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: 'none',
+    },
+  },
+})
+app.mount('#app')
