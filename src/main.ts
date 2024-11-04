9efa5d6b02371bfc520f1457845cbd9118073d6e
stugnait <teslitskii.link@gmail.com>
2024-11-05 00:41:41 +0200

    icons imported + Toast mounted

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import Aura from '@primevue/themes/aura'
 import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
+
+app.use(ToastService)
 
 app.use(PrimeVue, {
   theme: {
