ec79d6145e130d18b437ece8657b037cc24e46e4
stugnait <teslitskii.link@gmail.com>
2024-11-05 21:58:13 +0200

    Second Rework Update

--- src/components/AppItemList.vue
+++ src/components/AppItemList.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
-import type { Item } from '../types/model.types'
-import { defineComponent } from 'vue'
-import AppItemCard from './AppItemCard.vue'
+import { defineComponent } from 'vue';
+import type { Item } from '../types/model.types';
+import AppItemCard from './AppItemCard.vue';
 
 export default defineComponent({
   name: 'AppItemList',
@@ -14,19 +14,31 @@ export default defineComponent({
       required: true,
     },
   },
-  emits: ['itemClick', 'itemWishStatusChange', 'itemAddToCart'],
-  methods: {
-    onItemClick(itemId: number) {
-      this.$emit('itemClick', itemId)
-    },
-    onItemWishStatusChange(itemId: number, isWish: boolean) {
-      this.$emit('itemWishStatusChange', itemId, isWish)
-    },
-    onItemAddToCart(itemId: number) {
-      this.$emit('itemAddToCart', itemId)
-    },
+  emits: {
+    'itemClick': (itemId: number) => true,
+    'itemWishStatusChange': (itemId: number, isWish: boolean) => true,
+    'itemAddToCart': (itemId: number) => true,
   },
-})
+  setup(props, { emit }) {
+    const onItemClick = (itemId: number) => {
+      emit('itemClick', itemId);
+    };
+
+    const onItemWishStatusChange = (itemId: number, isWish: boolean) => {
+      emit('itemWishStatusChange', itemId, isWish);
+    };
+
+    const onItemAddToCart = (itemId: number) => {
+      emit('itemAddToCart', itemId);
+    };
+
+    return {
+      onItemClick,
+      onItemWishStatusChange,
+      onItemAddToCart,
+    };
+  },
+});
 </script>
 
 <template>
@@ -34,10 +46,10 @@ export default defineComponent({
     <div class="grid">
       <div v-for="item in items" :key="item.id" class="grid-item">
         <AppItemCard
-          :item="item"
-          @item-click="onItemClick"
-          @item-wish-status-change="onItemWishStatusChange"
-          @item-add-to-cart="onItemAddToCart"
+            :item="item"
+            @itemClick="onItemClick"
+            @itemWishStatusChange="onItemWishStatusChange"
+            @itemAddToCart="onItemAddToCart"
         />
       </div>
     </div>
@@ -51,8 +63,8 @@ export default defineComponent({
 
 .grid {
   display: grid;
-  grid-template-columns: repeat(4, 1fr); /* Три елементи в ряд */
-  gap: 1.5rem; /* Відступи між елементами */
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1.5rem;
 }
 
 .grid-item {
