7f20aea3037a3480fc4d49bcd65c0ee6dcce8a99
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2024-11-15 05:28:15 +0200

    feature: implement api for getting categories

--- src/api/category.api.ts
+++ src/api/category.api.ts
@@ -1,17 +1,38 @@
 import type { ItemCategory } from '../types/model.types'
+import axios from 'axios'
 
 const DELAY = 1000
-const mockCategories: ItemCategory[] = [
-  { id: 1, name: 'Electronics' },
-  { id: 2, name: 'Books' },
-  { id: 3, name: 'Clothing' },
-  { id: 4, name: 'Garden' },
-]
+// const mockCategories: ItemCategory[] = [
+//   { id: 1, name: 'Electronics' },
+//   { id: 2, name: 'Books' },
+//   { id: 3, name: 'Clothing' },
+//   { id: 4, name: 'Garden' },
+// ]
+
+// export function getAllCategories(): Promise<ItemCategory[]> {
+//   return new Promise((res, rej) => setTimeout(() => {
+//     Math.random() < 0.7
+//       ? res(mockCategories)
+//       : rej(new Error('Server is down'))
+//   }, DELAY))
+// }
 
 export function getAllCategories(): Promise<ItemCategory[]> {
-  return new Promise((res, rej) => setTimeout(() => {
-    Math.random() < 0.7
-      ? res(mockCategories)
-      : rej(new Error('Server is down'))
-  }, DELAY))
+  return new Promise((res, rej) => {
+    setTimeout(async () => {
+      try {
+        const response = await axios.get('https://api.escuelajs.co/api/v1/categories')
+        const categories = response.data.map((category: any) => ({
+          id: category.id,
+          name: category.name,
+          imageUrl: category.image,
+        }))
+        res(categories)
+      }
+      catch (error) {
+        console.error(error)
+        rej(new Error('Server is down'))
+      }
+    }, DELAY)
+  })
 }
