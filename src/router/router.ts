d8d9709298eef0b3c50759474391ccc9926424e4
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2024-11-16 03:16:28 +0200

    feature: initialize item page and wishlist routes

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppCatalog from '../views/AppCatalog.vue'
+import AppItemPage from '../views/AppItemPage.vue'
 import AppNotFound from '../views/AppNotFound.vue'
+import AppWishList from '../views/AppWishList.vue'
 
 const routes = [
   { path: '/', component: AppCatalog },
   { path: '/:pathMatch(.*)*', component: AppNotFound },
+  { path: '/item/:id', component: AppItemPage },
+  { path: '/wish-list', component: AppWishList },
 ]
 
 const router = createRouter({
---
+++ src/views/AppItemPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center min-h-[80vh] gap-6">
+    <p>x</p>
+  </div>
+</template>
