06e3b674d56aff6f47363fcc422cb16c7f1f392d
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-11-18 09:20:47 +0200

    Style the look of empty cart

--- src/views/AppCart.vue
+++ src/views/AppCart.vue
@@ -5,16 +5,19 @@ import AppCartItemList from '@/components/AppCartItemList.vue'
 import AppCartTotal from '@/components/AppCartTotal.vue'
 import useLoading from '@/composables/useLoading'
 import { addItemToCart, removeItemFromCart } from '@/storage/cart.storage'
+import Button from 'primevue/button'
 import Toast from 'primevue/toast'
 import { useToast } from 'primevue/usetoast'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
 const toast = useToast()
 
 const cartItems = ref<CartItem[]>([])
 const total = ref<number>(0)
 
-const isLoading = useLoading({
+const isCartLoading = useLoading({
   watcher: async () => {
     const cartInfo = await getCartInfo()
     cartItems.value = cartInfo.items
@@ -66,12 +69,25 @@ function onCheckout() {
 <template>
   <Toast />
   <div class="container mt-4">
-    <h1>Кошик</h1>
-    <AppCartItemList
-      :items="cartItems"
-      :is-loading="isLoading"
-      @item-amount-change="onItemAmountChange"
-    />
-    <AppCartTotal v-if="!isLoading && cartItems.length > 0" :total="total" @checkout="onCheckout" />
+    <div v-if="cartItems.length === 0 && !isCartLoading" class="text-center mt-[100px] text-4xl font-bold">
+      <p class="mb-10">
+        Your Cart is empty!
+      </p>
+      <Button
+        label="Go to Catalog"
+        icon="pi pi-shopping-bag"
+        severity="primary"
+        @click="router.push('/chameleon/')"
+      />
+    </div>
+    <div v-else>
+      <h1>Кошик</h1>
+      <AppCartItemList
+        :items="cartItems"
+        :is-loading="isCartLoading"
+        @item-amount-change="onItemAmountChange"
+      />
+      <AppCartTotal v-if="!isCartLoading && cartItems.length > 0" :total="total" @checkout="onCheckout" />
+    </div>
   </div>
 </template>
